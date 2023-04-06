d096900d91d7253a6df18b2911d89caa4886c52b
吴耀武 <wuyuw15@163.com>
2023-04-06 15:36:32 +0800

    fix: update CI

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,15 +15,12 @@ read -r -d '' usage <<- EOF
 use:
   bash deploy/deploy.sh [options]
 args:
-  -e      部署环境[test, prod]
   -v      部署版本(x.x.x)
 EOF
 
 # 参数
 while getopts e:v: OPT; do
   case "$OPT" in
-  e) opt_env=${OPTARG}
-    ;;
   v) opt_version=${OPTARG}
     ;;
   \?)
@@ -33,30 +30,20 @@ while getopts e:v: OPT; do
   esac
 done
 
-# 环境校验
-APP_ENV=('dev' 'test' 'prod')
-valid_env=0
-for i in "${APP_ENV[@]}"
-  do
-    if [[ ${i} == "$opt_env" ]]; then
-      valid_env=1
-    fi
-  done
-
 #对必填项做输入检查
 # shellcheck disable=SC2039
-if [[ $valid_env == 0 || -z $opt_version  ]]; then
+if [[ -z $opt_version  ]]; then
   echo "$usage"
-  echo "env: $opt_env, version: $opt_version"
+  echo "version: $opt_version"
   exit 1
 fi
 
 echo "参数解析成功！"
-echo "env: $opt_env, version: $opt_version"
+echo "version: $opt_version"
 
-sed -i "s#wuyuw/uam-admin-fe.*#wuyuw/uam-admin-fe-$opt_env:$opt_version#g" "${SCRIPTDIR}"/docker-compose.yaml
+sed -i "s#wuyuw/uam-admin-fe:.*#wuyuw/uam-admin-fe:$opt_version#g" "${SCRIPTDIR}"/docker-compose.yaml
 
 echo "docker-compose.yaml 配置完成"
 cat "${SCRIPTDIR}"/docker-compose.yaml
-docker-compose -f "${SCRIPTDIR}"/docker-compose.yaml -p uam-admin-fe-"$opt_env" up -d
+docker-compose -f "${SCRIPTDIR}"/docker-compose.yaml -p uam-admin-fe up -d
 echo "部署成功！"
